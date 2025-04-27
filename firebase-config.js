9e21d67470870dbbdd87bb64c0b3f0e197bbd0b7
Bhargav Chaudhari <118982442+bhargavlc2003@users.noreply.github.com>
2025-04-27 15:36:31 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyALjpwimVEe_ZDOSjyShJXbZhxPLqbv-gc",
-  authDomain: "doorguard-d5d1e.firebaseapp.com",
-  projectId: "doorguard-d5d1e",
-  storageBucket: "doorguard-d5d1e.firebasestorage.app",
-  messagingSenderId: "51813176200",
-  appId: "1:51813176200:web:bbceb9a11bf420d743e03d"
-};
-
+export const firebaseConfig = {
+    apiKey: "AIzaSyALjpwimVEe_ZDOSjyShJXbZhxPLqbv-gc",
+    authDomain: "doorguard-d5d1e.firebaseapp.com",
+    projectId: "doorguard-d5d1e",
+    storageBucket: "doorguard-d5d1e.firebasestorage.app",
+    messagingSenderId: "51813176200",
+    appId: "1:51813176200:web:bbceb9a11bf420d743e03d"
+}
